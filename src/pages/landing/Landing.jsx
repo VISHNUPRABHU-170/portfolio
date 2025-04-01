f269093b4bd243bad0c7f7e31e1763ac11963865
VISHNUPRABHU-170 <vishnurr1708@gmail.com>
2025-04-01 20:12:26 +0530

    Fixed Image not loaded issue

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,28 +1,28 @@
 import { useState, useEffect } from "react";
 import Image from "../../components/image/Image";
 import Link from "../../components/link/Link";
+import LandingImage1 from "/public/landing-1.svg";
+import LandingImage2 from "/public/landing-2.svg";
+import LandingImage3 from "/public/landing-3.svg";
+import LandingImage4 from "/public/landing-4.svg";
 import "./Landing.css";
 
 export default function Landing() {
-
-  const [imagePath, setImagePath] = useState(1);
+  const images = [LandingImage1, LandingImage2, LandingImage3, LandingImage4]; // Store references
+  const [imageIndex, setImageIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setImagePath((val) => {
-        return val + 1 < 5 ? val + 1 : 1;
-      });
+      setImageIndex((prevIndex) => (prevIndex + 1) % images.length);
     }, 4000);
 
-    return () => clearInterval(interval); // Cleanup the interval
-  }, []);
-
+    return () => clearInterval(interval); // Cleanup interval
+  }, [images.length]);
 
   return (
     <div className="landing">
-
       <div className="landing-image">
-        <Image imagePath={`/public/landing-${imagePath}.svg`} alt="Landing-Image" className="landing__img" />
+        <Image imagePath={images[imageIndex]} alt="Landing-Image" className="landing__img" />
       </div>
 
       <div className="landing-content">
@@ -31,7 +31,6 @@ export default function Landing() {
         <span className="landing-text">Full Stack Developer</span>
         <Link label="My Resume" />
       </div>
-
     </div>
   );
 }
